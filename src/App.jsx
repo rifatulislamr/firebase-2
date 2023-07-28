d572bc0610432ac749d68bdd367bcf1570ec9d43
rifatulislamr <rifatulcse@gmail.com>
2023-07-28 07:55:32 +0600

    add this code

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='uppercase mt-16 text-center bg-zinc-100 rounded-sm mr-px text-rose-700'>My name is rifatul islam riad</h1>
+      <button className="btn btn-active">Default</button>
+<button className="btn btn-active btn-neutral">Neutral</button>
+<button className="btn btn-active btn-primary">Primary</button>
+<button className="btn btn-active btn-secondary">Secondary</button>
+<button className="btn btn-active btn-accent">Accent</button>
+<button className="btn btn-active btn-ghost">Ghost</button>
+<button className="btn btn-active btn-link">Link</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,57 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import app from "../../firebase/firebase.init";
+import { useState } from "react";
+
+
+
+
+const Login = () => {
+
+    const [user,setUser] = useState(null);
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () =>{
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const loggedInUser = result.user;
+            console.log(loggedInUser);
+            setUser(loggedInUser);
+
+        })
+        .catch(error =>{
+            console.log('error', error.message)
+
+        })
+    
+ }
+ const handleSignOut = () =>{
+    signOut(auth)
+    .then(result =>{
+        console.log(result);
+        setUser(null);
+
+    })
+    .catch(error =>{
+        console.log(error)
+    })
+}
+  
+    
+    return (
+        <div>
+        {   user ?
+            <button onClick={handleSignOut} className="btn btn-accent">Sing Out</button> :
+            <button onClick={handleGoogleSignIn} className="btn btn-accent">Google Login</button>
+        }
+          
+        { user &&<div>
+        <h3>User: {user.displayName} </h3>
+        <p>Email: {user.email} </p>
+        <img className=" relative" src={user.photoURL} alt="" />
+        </div>}
+        </div>
+    );
+};
+
+export default Login;
